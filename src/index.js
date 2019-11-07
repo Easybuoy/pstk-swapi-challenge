f29725c86705744518e0ac62e964589d726c0f82
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-11-07 23:29:45 +0100

    [finishes #169636326] Install dependencies

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 
 import store from './store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
+    <ToastContainer
+      autoClose={3000}
+      position="top-right"
+      hideProgressBar={true}
+    />
   </Provider>,
   document.getElementById('root')
 );
