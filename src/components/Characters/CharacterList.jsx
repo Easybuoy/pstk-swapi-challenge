3061c74b007a7a5fdc300ac592cbded805d15393
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-11-08 12:04:04 +0100

    add iability to get single movie detail

--- src/components/Characters/CharacterList.jsx
+++ src/components/Characters/CharacterList.jsx
@@ -2,13 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import { getMovie } from '../../actions';
 import StarWarsImage from '../../assets/images/star-wars.png';
 
-const CharacterList = ({ selectedMovie, getMovie }) => {
-  console.log(selectedMovie);
+const CharacterList = ({ selectedMovie, movie }) => {
   if (!selectedMovie) {
-    console.log('eee');
     return (
       <div>
         <img
@@ -20,23 +17,23 @@ const CharacterList = ({ selectedMovie, getMovie }) => {
     );
   }
 
-//   getMovie();
   return <div>Character List</div>;
 };
 
 CharacterList.propTypes = {
   loading: PropTypes.object.isRequired,
-  error: PropTypes.object.isRequired
+  error: PropTypes.object.isRequired,
+  movie: PropTypes.object.isRequired,
+  movies: PropTypes.array.isRequired,
+  selectedMovie: PropTypes.string.isRequired
 };
 
 const mapStateToProps = state => ({
   loading: state.loading,
   error: state.error,
   movies: state.swapi.movies,
+  movie: state.swapi.movie,
   selectedMovie: state.swapi.selectedMovie
 });
 
-export default connect(
-  mapStateToProps,
-  { getMovie }
-)(CharacterList);
+export default connect(mapStateToProps)(CharacterList);
