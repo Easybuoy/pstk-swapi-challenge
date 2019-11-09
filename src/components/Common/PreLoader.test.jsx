ebc5f753a72a38d3ae9068d86a573b276faa60b1
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-11-09 12:27:46 +0100

    install dependencies for coverage

--- src/components/Common/PreLoader.test.jsx
+++ src/components/Common/PreLoader.test.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-// import sinon from 'sinon';
 
 import PreLoader from './PreLoader';
 
 describe('<PreLoader />', () => {
-
-        it('renders the Cart component correctly', () => {
-            const wrapper = shallow(<PreLoader />);
-            expect(wrapper).toMatchSnapshot();
-        });
+  it('renders the PreLoader component correctly', () => {
+    const wrapper = shallow(<PreLoader />);
+    expect(wrapper).toMatchSnapshot();
+  });
 });
