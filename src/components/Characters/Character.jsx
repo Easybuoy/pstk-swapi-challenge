68a32d129866dea5c1b211949c7fa86f49f49283
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-11-15 15:27:31 +0100

    rfinishes #169775398 ] eremove unecessary dependencies

--- src/components/Characters/Character.jsx
+++ src/components/Characters/Character.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 
-function Character({ name, gender, height }) {
+const Character = ({ name, gender, height }) => {
   return (
-    // <div>
     <tr>
       <td>{name}</td>
       <td>{gender}</td>
       <td>{height}</td>
     </tr>
-    // </div>
   );
-}
+};
 
 export default Character;
