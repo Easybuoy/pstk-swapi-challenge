4d72969db01aa66c7a851085b235bfd6065d3676
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-11-15 11:10:12 +0100

    remove toastify usage

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'react-toastify/dist/ReactToastify.min.css';
 import './index.css';
 
 ReactDOM.render(
